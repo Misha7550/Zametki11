deb0d67b890870faa2e28bb6a9b43987ec76ff79
Misha7550 <104518437+Misha7550@users.noreply.github.com>
2022-04-27 20:09:27 +0300

    Добавьте файлы проекта.

---
+++ Zametki/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Zametki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Text = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.zametkiBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database1DataSet = new Zametki.Database1DataSet();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.zametkiTableAdapter = new Zametki.Database1DataSetTableAdapters.ZametkiTableAdapter();
+            this.but_add = new System.Windows.Forms.Button();
+            this.but_del = new System.Windows.Forms.Button();
+            this.but_save = new System.Windows.Forms.Button();
+            this.tableAdapterManager = new Zametki.Database1DataSetTableAdapters.TableAdapterManager();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zametkiBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Khaki;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.Text});
+            this.dataGridView1.DataSource = this.zametkiBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(140, 358);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // Text
+            // 
+            this.Text.DataPropertyName = "Text";
+            this.Text.HeaderText = "Text";
+            this.Text.Name = "Text";
+            this.Text.Visible = false;
+            // 
+            // zametkiBindingSource
+            // 
+            this.zametkiBindingSource.DataMember = "Zametki";
+            this.zametkiBindingSource.DataSource = this.database1DataSet;
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Khaki;
+            this.richTextBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.zametkiBindingSource, "Text", true));
+            this.richTextBox1.Location = new System.Drawing.Point(158, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(214, 358);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // zametkiTableAdapter
+            // 
+            this.zametkiTableAdapter.ClearBeforeFill = true;
+            // 
+            // but_add
+            // 
+            this.but_add.Location = new System.Drawing.Point(12, 5);
+            this.but_add.Name = "but_add";
+            this.but_add.Size = new System.Drawing.Size(80, 25);
+            this.but_add.TabIndex = 2;
+            this.but_add.Text = "Добавить";
+            this.but_add.UseVisualStyleBackColor = true;
+            this.but_add.Click += new System.EventHandler(this.but_add_Click);
+            // 
+            // but_del
+            // 
+            this.but_del.Location = new System.Drawing.Point(98, 6);
+            this.but_del.Name = "but_del";
+            this.but_del.Size = new System.Drawing.Size(96, 24);
+            this.but_del.TabIndex = 3;
+            this.but_del.Text = "Удалить";
+            this.but_del.UseVisualStyleBackColor = true;
+            this.but_del.Click += new System.EventHandler(this.but_del_Click);
+            // 
+            // but_save
+            // 
+            this.but_save.Location = new System.Drawing.Point(200, 6);
+            this.but_save.Name = "but_save";
+            this.but_save.Size = new System.Drawing.Size(98, 24);
+            this.but_save.TabIndex = 4;
+            this.but_save.Text = "Сохранить";
+            this.but_save.UseVisualStyleBackColor = true;
+            this.but_save.Click += new System.EventHandler(this.but_save_Click);
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.UpdateOrder = Zametki.Database1DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            this.tableAdapterManager.ZametkiTableAdapter = this.zametkiTableAdapter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Khaki;
+            this.textBox1.Location = new System.Drawing.Point(340, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(32, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(122, 48);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(121, 22);
+            this.toolStripMenuItem1.Text = "Открыть";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(121, 22);
+            this.toolStripMenuItem2.Text = "Выход";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Заметки";
+            this.notifyIcon1.Visible = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::Zametki.Properties.Resources.Pinboard_icon_icons_com_66981__1_;
+            this.button1.Location = new System.Drawing.Point(304, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 28);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(386, 419);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.but_save);
+            this.Controls.Add(this.but_del);
+            this.Controls.Add(this.but_add);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zametkiBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource zametkiBindingSource;
+        private Database1DataSetTableAdapters.ZametkiTableAdapter zametkiTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Text;
+        private System.Windows.Forms.Button but_add;
+        private System.Windows.Forms.Button but_del;
+        private System.Windows.Forms.Button but_save;
+        private Database1DataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+    }
+}
+
---
+++ Zametki/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Drawing.Drawing2D;
+using Microsoft.Data.Sqlite;
+using System.IO;
+using System.Data.SQLite;
+
+
+
+namespace Zametki
+{
+    public partial class Form1 : Form
+    {
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.AllowUserToAddRows = false;
+            notifyIcon1.Visible = true;
+            ShowInTaskbar = false;
+            textBox1.Text = "выкл";
+
+
+            
+
+
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "database1DataSet.Zametki". При необходимости она может быть перемещена или удалена.
+            this.zametkiTableAdapter.Fill(this.database1DataSet.Zametki);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "database1DataSet1.Zametki". При необходимости она может быть перемещена или удалена.
+            
+
+
+        }
+
+        
+
+        private void but_add_Click(object sender, EventArgs e)
+        {
+            DataRow row = database1DataSet.Tables[0].NewRow(); // добавляем новую строку в DataTable
+            database1DataSet.Tables[0].Rows.Add(row);
+        }
+
+        private void but_del_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                dataGridView1.Rows.Remove(row);
+            }
+        }
+
+        private void but_save_Click(object sender, EventArgs e)
+        {
+            zametkiBindingSource.EndEdit();
+            zametkiTableAdapter.Update(database1DataSet);
+            MessageBox.Show("Изменения успешно сохранены");
+
+
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Normal;
+                this.ShowInTaskbar = true;
+                notifyIcon1.Visible = false;
+            }
+        }
+
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public bool www;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+
+            String value = (String)key.GetValue("Zametki");
+
+            if (!String.IsNullOrEmpty(value))
+            {
+                www = false;
+            }
+                // открываем нужную ветку в реестре 
+                // @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run\"
+                if (www == true)
+            {
+
+                //добавляем первый параметр - название ключа
+                // Второй параметр - это путь к 
+                // исполняемому файлу нашей программы.
+                key.SetValue("Zametki", Application.ExecutablePath);
+                www = false;
+                textBox1.Text = "вкл";
+            }
+            else
+            {
+                //удаляем
+                key.DeleteValue("Zametki", false);
+                
+                www = true;
+                textBox1.Text = "выкл";
+            }
+            key.Close();
+        }
+
+        
+        
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            WindowState = FormWindowState.Normal;
+
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+}
